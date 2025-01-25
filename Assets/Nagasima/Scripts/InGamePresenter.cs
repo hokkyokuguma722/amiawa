742435dcc1e37ac651d493ecac6bd4d570370c73
LAPTOP-83635360\admin <m0a22221d0@edu,teu.ac.jp>
2025-01-25 11:51:17 +0900

    コードレビューの結果を反映しました

--- Assets/Nagasima/Scripts/InGamePresenter.cs
+++ Assets/Nagasima/Scripts/InGamePresenter.cs
@@ -6,6 +6,15 @@ public class InGamePresenter : IPresenter
     private InGameView inGameView;
     private PresenterChanger presenterChanger;
 
+    public InGamePresenter(InGameModel model, InGameView view, PresenterChanger pChanger)
+    {
+        inGameModel = model;
+        inGameView = view;
+        presenterChanger = pChanger;
+
+        inGameView.Show();
+    }
+
     public void Initialize()
     {
         Debug.Log("InGamePresenterを初期化");
@@ -14,21 +23,12 @@ public class InGamePresenter : IPresenter
     public void Show()
     {
         Debug.Log("インゲーム画面を表示");
-        inGameView.Show();
+        inGameView.gameObject.SetActive(true);
     }
 
     public void Hide()
     {
         Debug.Log("インゲーム画面を非表示");
-        inGameView.Hide();
-    }
-
-    public InGamePresenter(InGameModel model, InGameView view, PresenterChanger pChanger)
-    {
-        inGameModel = model;
-        inGameView = view;
-        presenterChanger = pChanger;
-
-        inGameView.Show();
+        inGameView.gameObject.SetActive(false);
     }
 }
