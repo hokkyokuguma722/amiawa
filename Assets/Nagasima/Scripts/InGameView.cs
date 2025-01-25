5693541d0729d12c875ac7a5339197827a03e78e
KoukiNakata <88617991+KoukiNakata@users.noreply.github.com>
2025-01-25 17:28:13 +0900

    inGameView更新

--- Assets/Nagasima/Scripts/InGameView.cs
+++ Assets/Nagasima/Scripts/InGameView.cs
@@ -3,19 +3,24 @@ using UnityEngine.UI;
 
 public interface IInGameView : IView
 {
-    void SetVoiceImage(float scale);
+    void UpdateSpeechBubbleImage(float scale, float spectrum);
+    public void UpdateVoiceRecorderImage(float scale);
+    public void UpdateMouthImage(float scale);
+    public void UpdateNeedleImage(float volume);
 }
 
 public class InGameView : MonoBehaviour, IInGameView
 {
-    [SerializeField] private Image ingameImage;
-
+    [SerializeField] private Image[] sceneImages;
     [SerializeField] private Image voiceRecorderImage;
+    [SerializeField] private Image mouthImage;
+    [SerializeField] private Image speechBubbleImage;
+    [SerializeField] private Sprite[] speechBubbleSprites;
+    [SerializeField] private GameObject needleImage;
 
-    [SerializeField] private Image storyImage;
-
-    [SerializeField] private Image VoiceImage;
-
+    private const float MaxAngle = 45.0f; // 最大角度（度数法）
+    private float currentAngle = 0.0f; // 現在の角度
+    private bool isIncreasing = true; // 角度が増加中かどうか
 
     public void Initialize()
     {
@@ -31,19 +36,80 @@ public class InGameView : MonoBehaviour, IInGameView
         gameObject.SetActive(false);
     }
 
-    public void Frame(float framesize)
+    /// <summary>
+    /// ボイスの音量を可視化するView
+    /// </summary>
+    /// <param name="scale"></param>
+    public void UpdateVoiceRecorderImage(float scale)
     {
-        ingameImage.transform.localScale = Vector3.right * framesize;
-        ingameImage.transform.localScale = Vector3.up * framesize;
+        voiceRecorderImage.transform.localScale = new Vector3(scale, 1, 1);
     }
 
-    public void SetVoiceImage(float scale)
+    /// <summary>
+    /// 吹き出しのサイズを調整
+    /// </summary>
+    /// <param name="scale"></param>
+    public void UpdateSpeechBubbleImage(float scale, float spectrum)
     {
-        VoiceImage.transform.localScale = new Vector3(1, scale, 1);
+        //大きさを変形
+        speechBubbleImage.transform.localScale = new Vector3(1, scale, 1);
+
+
+        //TODO:数値は仮
+        //形状を変化
+        if (spectrum > 2)
+        {
+            speechBubbleImage.sprite = speechBubbleSprites[0];
+        }
+        else if (spectrum > 1)
+        {
+            speechBubbleImage.sprite = speechBubbleSprites[1];
+        }
+        else
+        {
+            speechBubbleImage.sprite = speechBubbleSprites[2];
+        }
     }
 
-    public void VoiceRecorder(float voiderecordersize)
+    /// <summary>
+    /// キャラクターの口の動き
+    /// </summary>
+    /// <param name="scale"></param>
+    public void UpdateMouthImage(float scale)
     {
-        voiceRecorderImage.transform.localScale = Vector3.right * voiderecordersize;
+        voiceRecorderImage.transform.localScale = new Vector3(1 + scale, 1 + scale, 1);
+    }
+
+    /// <summary>
+    /// 針の振れの動き
+    /// </summary>
+    /// <param name="volume"></param>
+    public void UpdateNeedleImage(float volume)
+    {
+        void Update()
+        {
+            // 角度を更新
+            if (isIncreasing)
+            {
+                currentAngle += volume;
+                if (currentAngle >= MaxAngle)
+                {
+                    currentAngle = MaxAngle;
+                    isIncreasing = false; // 減少に切り替え
+                }
+            }
+            else
+            {
+                currentAngle -= volume;
+                if (currentAngle <= 0)
+                {
+                    currentAngle = 0;
+                    isIncreasing = true; // 増加に切り替え
+                }
+            }
+
+            // 針を回転
+            needleImage.transform.rotation = Quaternion.Euler(0, 0, currentAngle);
+        }
     }
 }
