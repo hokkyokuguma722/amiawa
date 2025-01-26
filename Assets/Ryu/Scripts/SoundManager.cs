a27febe8a6e295f00c363b62248465baa12bb475
Ryuchisei <m0a2329750@edu.teu.ac.jp>
2025-01-26 10:33:00 +0900

    [add] BGMの設定を追加

--- Assets/Ryu/Scripts/SoundManager.cs
+++ Assets/Ryu/Scripts/SoundManager.cs
@@ -20,11 +20,33 @@ public class SoundManager : MonoBehaviour
         }
     }
 
-    public AudioSource audioSourceSE;
-    public AudioClip[] audioCllipsSE;
+    public AudioSource audioSourceBGM;
+    public AudioClip[] audioClipsBGM;
 
+    public AudioSource audioSourceSE;
+    public AudioClip[] audioClipsSE;
+
+    public void PlayBGM(SceneType sceneType)
+    {
+        audioSourceBGM.Stop();
+        switch(sceneType)
+        {
+            default:
+            case SceneType.FristScene:
+            case SceneType.SecondScene:
+               audioSourceBGM.clip = audioClipsBGM[0];
+                break;
+            case SceneType.ThridScene:
+                audioSourceBGM.clip = audioClipsBGM[1];
+                break;
+            case SceneType.ForthScene:
+                audioSourceBGM.clip = audioClipsBGM[2];
+                break;
+        }
+        audioSourceBGM.Play();
+    }
     public void PalySE(int index)
     {
-        audioSourceSE.PlayOneShot(audioCllipsSE[index]);
+        audioSourceSE.PlayOneShot(audioClipsSE[index]);
     }
 }
